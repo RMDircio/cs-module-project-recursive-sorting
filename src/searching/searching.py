0713b090ca21abd45d35d5776ec36723578c1b6a
RMDircio <reginadircio@gmail.com>
2020-10-08 20:33:08 -0500

    Assignment Complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,23 +1,30 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # set the middle
-    middle_index = (end-start) // 2
+    middle_index = (end - start) // 2 + start
+    # print(f"{middle_index} = ({end} - {start}) // 2 + {start}")
+
+    # If the index is invalid (below zero) then the target was not found
+    if middle_index < 0:
+        return -1
 
     # check if target is the middle index
-    if arr[middle_index] == target:
-        return True
+    elif arr[middle_index] == target:
+        return middle_index
     
-    # check is middle index is 0
-    elif arr[middle_index] == 0:
-        return False
+    # check is middle index is first index
+    elif middle_index == start:
+        if arr[end] == target:
+            return end
+        else:
+            return -1
     
     # if middle index is less than target
     elif arr[middle_index] < target:
         return binary_search(arr, target, middle_index, end)
     
     # if middle index is greater than target
-    else:
-        if arr[middle_index] > target:
+    elif arr[middle_index] > target:
             return binary_search(arr, target, start, middle_index)
 
 
